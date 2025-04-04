05d9f323a865203f5f491daf8ae67e413d7248b8
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-04-04 16:07:20 -0300

    project creating structure

---
+++ src/main/java/dio/abel/springboot_jwt/DioSpringbootJwtApplication.java
@@ -0,0 +1,13 @@
+package dio.abel.springboot_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/abel/springboot_jwt/DioSpringbootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.abel.springboot_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
