ee8f1b778c46191faa7b8919f150937510971d83
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-04-04 16:16:38 -0300

    chore: renamed project launchers

--- src/main/java/dio/abel/springboot_jwt/Main.java
+++ src/main/java/dio/abel/springboot_jwt/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DioSpringbootJwtApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DioSpringbootJwtApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
