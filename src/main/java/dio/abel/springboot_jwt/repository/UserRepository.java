7ee5d43e48546e5a41b88ef5dc97a8743c808833
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-04-04 17:02:28 -0300

    feat: repository user created

---
+++ src/main/java/dio/abel/springboot_jwt/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package dio.abel.springboot_jwt.repository;
+
+import dio.abel.springboot_jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select e from users e join fetch e.roles where e.username= (:username)")
+    User findByUsername(@Param("username") String username);
+
+    boolean existsByUsername(String username);
+}
